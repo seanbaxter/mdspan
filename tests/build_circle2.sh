a8a11ec50fccb5b8d3971ef21c17da65a5bee760
Sean Baxter <lightborn@gmail.com>
2021-12-26 22:42:08 -0500

    Adopted prefix notation for pack members

--- tests/build_circle2.sh
+++ tests/build_circle2.sh
@@ -1,9 +1,9 @@
 set -x 
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_contiguous_layouts.cpp
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_element_access.cpp
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_extents.cpp
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_layout_ctors.cpp
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_layout_stride.cpp
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_mdspan_conversion.cpp
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_mdspan_ctors.cpp
-circle -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_submdspan.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_contiguous_layouts.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_element_access.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_extents.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_layout_ctors.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_layout_stride.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_mdspan_conversion.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_mdspan_ctors.cpp
+circle -std=c++20 -P2128 -O0 -I ../circle -lgtest -lgtest_main -lpthread test_submdspan.cpp
